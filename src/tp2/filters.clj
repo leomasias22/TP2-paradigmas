11582856c50150235a617bd4dd91ec78c41cfab9
LucaFog <151969276+LucaFog@users.noreply.github.com>
2026-05-28 15:27:02 -0300

    Filtros Brillo + Invertir
    Futuro: Blur [3x3]

--- src/tp2/filters.clj
+++ src/tp2/filters.clj
@@ -1,15 +1,42 @@
-(ns tp2.filters)
+(ns tp2.filters
+  (:require [tp2.imagenes :as img]))
 
-"Funciones puras de procesamiento de imagen."
+; "Funciones puras de procesamiento de imagen."
 
-(defn filtro_1
-  "un filtro puntual por píxel (por ejemplo desaturar, invertir, brillo, colorizar)"
-  (println "Se ejecutó el filtro 1"))
+(defn brillo-pixel [pixel factor-brillo]
+  ; Dado un pixel [un map de 4 campos], devuelve un pixel con brillo aumentado.
+  {:a (get pixel :a)
+   :r (img/clamp (* (get pixel :r) factor-brillo))
+   :g (img/clamp (* (get pixel :g) factor-brillo))
+   :b (img/clamp (* (get pixel :b) factor-brillo))
+   })
 
-(defn filtro_2
-  "un filtro por vecindad o convolución (por ejemplo difuminado, realce, detección de bordes))"
-  (println "Se ejecutó el filtro 2"))
+(defn brillo [matriz-pixeles]
+  ; Aumenta el brillo de la imagen, multiplicando una constante pixel por pixel.
+  (let [factor-brillo 1.1]
+    (mapv (fn [fila-pixeles]
+            (mapv (fn [pixel] (brillo-pixel pixel factor-brillo)) fila-pixeles))
+          matriz-pixeles)
+    ))
 
-(defn filtro_3
-  "un tercer filtro libre a elección del grupo"
-  (println "Se ejecutó el filtro 3"))
+(defn blur [matriz-pixeles]
+  ; Difumina la imagen, reduciendo detalles finos y el ruido local.
+  ; Este filtro usa un kernel de suavizado de caja 3x3.
+
+
+  )
+
+(defn invertir-pixel [pixel]
+  ; Dado un pixel [un map de 4 campos], devuelve un pixel con campos RGBA invertidos.
+  {:a (get pixel :a)
+   :r (- 255 (get pixel :r))
+   :g (- 255 (get pixel :g))
+   :b (- 255 (get pixel :b))
+   })
+
+(defn invertir [matriz-pixeles]
+  ; Invierte los colores de la imagen, modificando el RGB pixel por pixel
+  (mapv (fn [fila-pixeles]
+          (mapv (fn [pixel] (invertir-pixel pixel)) fila-pixeles))
+        matriz-pixeles)
+  )
--- src/tp2/imagenes.clj
+++ src/tp2/imagenes.clj
@@ -2,35 +2,45 @@
   (:import [javax.imageio ImageIO]
            [java.io File]))
 
-"Funciones para abrir / administrar imagenes"
+; "Funciones para abrir / administrar imagenes"
 
-(defn clamp
-  "Dado un entero, devuelve uno dentro del rango [0, 255]"
-  [num]
+(defn clamp [num]
+  ; Dado un entero que representa un valor de color, devuelve uno dentro del rango [0, 255]
   (cond
     (< num 0) 0
     (> num 255) 255
     :else num))
 
+(defn obtener_pixel [matriz_pixeles x y]
+  ; Dada una matriz de pixeles y unas coordenadas, devuelve un pixel.
+  ; En el caso de que esté fuera de los límites, devuelve un pixel RGBA #00000000.
+  (get (get matriz_pixeles x []) y 0x00000000))
+
 (defn cargar_imagen [filepath]
+  ; Dado un filepath, carga una imagen en memoria.
   (ImageIO/read (File. filepath)))
 
 (defn guardar_imagen [imagen extension filepath]
+  ; Dados una imagen, una extension de archivo (como .png, .jpg, etc) y un filepath, guarda una imagen en el disco.
   (ImageIO/write imagen extension (File. filepath)))
 
-(defn- desempaquetar_pixel [pixel]
-  "Dado un entero, extrae los canales RGB en un Map."
-  {:r (bit-and (bit-shift-right pixel 16) 0xFF)
+(defn desempaquetar_pixel [pixel]
+  ; Dado un entero, extrae los canales RGBA en un Map.
+  {:a (bit-and (bit-shift-right pixel 24) 0xFF)
+   :r (bit-and (bit-shift-right pixel 16) 0xFF)
    :g (bit-and (bit-shift-right pixel 8) 0xFF)
    :b (bit-and pixel 0xFF)})
 
 (defn obtener_pixeles [imagen]
-  "Dada una imagen, devuelve sus pixeles."
+  ; Dada una imagen, devuelve sus pixeles en una matriz de pixeles [que son mapas RGBA].
   (let [w (.getWidth imagen)
         h (.getHeight imagen)
         arreglo_pixeles (int-array (* w h))]
 
-  "https://docs.oracle.com/javase/8/docs/api/java/awt/image/BufferedImage.html#:~:text=int%29-,getRGB"
+  ; https://docs.oracle.com/javase/8/docs/api/java/awt/image/BufferedImage.html#:~:text=int%29-,getRGB
   (.getRGB imagen 0 0 w h arreglo_pixeles 0 w)
-  (map desempaquetar_pixel (seq arreglo_pixeles))
+  ; Transformar a forma matricial:
+  ; primero desempaquetar, luego particionar, finalmente vectorizar.
+  ; TODO: seguramente existirá algún metodo mejor
+  (mapv vec (partition-all w (mapv desempaquetar_pixel arreglo_pixeles)))
   ))
