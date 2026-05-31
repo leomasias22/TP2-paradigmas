c6d22c5157b271f7383b7bf5c5eac1067e1080ee
LucaFog <151969276+LucaFog@users.noreply.github.com>
2026-05-31 17:58:28 -0300

    Difuminado [bordes oscuros]
    Busco una solucion para los bordes oscuros, y luego envío

--- src/tp2/filters.clj
+++ src/tp2/filters.clj
@@ -19,12 +19,35 @@
           matriz-pixeles)
     ))
 
-(defn blur [matriz-pixeles]
+(defn avg-kernel [subvector-pixeles]
+  ; Dada una submatriz de pixeles, calcula el pixel promedio.
+  (let [canales [:a :r :g :b]]
+    ; Crear un mapa de formato adecuado [canales : sumas/9]
+    (zipmap canales
+            (for [canal canales]
+              (quot (reduce + (map canal subvector-pixeles)) 9)))
+    ; Es como hacer "(map #(quot (reduce + (map % subvector-pixeles)) 9) canales)".
+    ))
+
+(defn difuminado-pixel [matriz-pixeles x y]
+  ; Dada una matriz de pixeles y coordenadas de un pixel, calcula un subvector de pixeles,
+  ; luego delega al kernel la tarea de promedio de pixel, y devuelve su resultado.
+  (let [subvector-pixeles (for [ady-x [-1 0 1] ady-y [-1 0 1]]
+                            (img/obtener-pixel matriz-pixeles (+ x ady-x) (+ y ady-y))
+                            )]
+    (avg-kernel subvector-pixeles)
+  ))
+
+(defn difuminado [matriz-pixeles]
   ; Difumina la imagen, reduciendo detalles finos y el ruido local.
   ; Este filtro usa un kernel de suavizado de caja 3x3.
-
-
-  )
+  (let [alto (count matriz-pixeles)
+        ancho (count (first matriz-pixeles))]
+    (vec (for [y (range alto)]
+           (vec (for [x (range ancho)]
+                  (difuminado-pixel matriz-pixeles x y)))
+           ))
+    ))
 
 (defn invertir-pixel [pixel]
   ; Dado un pixel [un map de 4 campos], devuelve un pixel con campos RGBA invertidos.
--- src/tp2/imagenes.clj
+++ src/tp2/imagenes.clj
@@ -2,7 +2,7 @@
   (:import [javax.imageio ImageIO]
            [java.io File]))
 
-; "Funciones para abrir / administrar imagenes"
+; "Funciones para abrir / administrar imagenes y modelar pixeles"
 
 (defn clamp [num]
   ; Dado un entero que representa un valor de color, devuelve uno dentro del rango [0, 255]
@@ -10,11 +10,6 @@
     (< num 0) 0
     (> num 255) 255
     :else num))
-
-(defn obtener_pixel [matriz_pixeles x y]
-  ; Dada una matriz de pixeles y unas coordenadas, devuelve un pixel.
-  ; En el caso de que esté fuera de los límites, devuelve un pixel RGBA #00000000.
-  (get (get matriz_pixeles x []) y 0x00000000))
 
 (defn cargar_imagen [filepath]
   ; Dado un filepath, carga una imagen en memoria.
@@ -24,14 +19,26 @@
   ; Dados una imagen, una extension de archivo (como .png, .jpg, etc) y un filepath, guarda una imagen en el disco.
   (ImageIO/write imagen extension (File. filepath)))
 
-(defn desempaquetar_pixel [pixel]
+(defn desempaquetar-pixel [pixel]
   ; Dado un entero, extrae los canales RGBA en un Map.
   {:a (bit-and (bit-shift-right pixel 24) 0xFF)
    :r (bit-and (bit-shift-right pixel 16) 0xFF)
    :g (bit-and (bit-shift-right pixel 8) 0xFF)
    :b (bit-and pixel 0xFF)})
 
-(defn obtener_pixeles [imagen]
+(defn empaquetar-pixel [pixel]
+    (bit-or (bit-shift-left (:a pixel) 24)
+            (bit-shift-left (:r pixel) 16)
+            (bit-shift-left (:g pixel) 8)
+            (:b pixel)
+            ))
+
+(defn obtener-pixel [matriz-pixeles x y]
+  ; Dada una matriz de pixeles y unas coordenadas, devuelve un pixel.
+  ; En el caso de que esté fuera de los límites, devuelve un pixel RGBA #00000000.
+  (get (get matriz-pixeles x []) y (desempaquetar-pixel 0x00000000)))
+
+(defn obtener-pixeles [imagen]
   ; Dada una imagen, devuelve sus pixeles en una matriz de pixeles [que son mapas RGBA].
   (let [w (.getWidth imagen)
         h (.getHeight imagen)
@@ -39,8 +46,6 @@
 
   ; https://docs.oracle.com/javase/8/docs/api/java/awt/image/BufferedImage.html#:~:text=int%29-,getRGB
   (.getRGB imagen 0 0 w h arreglo_pixeles 0 w)
-  ; Transformar a forma matricial:
-  ; primero desempaquetar, luego particionar, finalmente vectorizar.
-  ; TODO: seguramente existirá algún metodo mejor
-  (mapv vec (partition-all w (mapv desempaquetar_pixel arreglo_pixeles)))
+  ; Transformar a forma matricial: primero desempaquetar, luego particionar, finalmente vectorizar.
+  (mapv vec (partition-all w (mapv desempaquetar-pixel arreglo_pixeles)))
   ))
