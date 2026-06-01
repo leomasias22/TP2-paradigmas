b46e4dc0559620068484b217a3cc68a97298dac5
LucaFog <151969276+LucaFog@users.noreply.github.com>
2026-06-01 20:14:22 -0300

    Paralelismo para pipelining
    Para administrar una capa de paralelismo y hacer que el proceso de imagenes no esté comprimido en un solo hilo

--- src/tp2/pipeline.clj
+++ src/tp2/pipeline.clj
@@ -1,8 +1,6 @@
 (ns tp2.pipeline)
 
 ; "Espacio para el pipeline, independiente de una imagen"
-; Las funciones son puras, salvo se indique lo contrario.
-; Requieren de un pipeline, y devuelven un pipeline nuevo
 
 (defn agregar-filtro [pipeline filtro]
   ; Agrega un filtro al final del pipeline.
@@ -18,20 +16,3 @@
 (defn reset-pipeline []
   ; Devuelve un pipeline vacio.
   [])
-
-(defn aplicar-pipeline [pipeline imagen-original]
-  ; Aplica toda la secuencia de efectos del pipeline sobre la imagen original, y devuelve la imagen resultado.
-  ; La imagen original debe ser una matriz de pixeles. La imagen resultado tendrá tambien formato de matriz.
-  (reduce (fn [imagen filtro] (filtro imagen))
-          imagen-original pipeline
-          ))
-
-; TODO/IDEAS PARALELISMO FILTROS:
-; En el namespace que administre los llamados (la UI), podría haber un bloque que se vea algo asi:
-; (actualizar el estado: se comenzaron a aplicar los filtros)
-; (future
-;  (let [imagen-final (pipeline/aplicar-pipeline pipeline imagen-actual)]
-;    (actualizar el estado: se terminaron de aplicar los filtros)
-;    ))
-
-; Asimismo, luego la UI se encargaría (CON PMAP) de dividir las tareas, y luego unir las imagenes que resulten.
